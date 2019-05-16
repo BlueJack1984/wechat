5d81eb27e5dfc50ad6b3a5872987dffec687617c
lushusheng <lushushen8@163.com>
2019-05-16 20:17:10 +0800

    commit

--- src/main/java/com/example/weixin/controller/security/SecurityController.java
+++ src/main/java/com/example/weixin/controller/security/SecurityController.java
@@ -22,56 +22,61 @@ public class SecurityController {
         return "/less/index.html";
     }
 
+    /**
+     * 用户登录
+     * @param loginInput
+     * @return
+     */
     @PostMapping("/login")
     public String login(@RequestBody @Valid LoginInput loginInput) {
 
         String loginInfo = JSONObject.toJSONString(loginInput);
 
         String result = HttpUtil.post("https://www.baidu.com", loginInfo);
+        if (currentUser == null) {
+            //登录失败
+            req.setAttribute("errorMsg", "亲,账户或者密码错误");
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+            return "";
+        } else {
+            //登录成功
+            req.getSession().setAttribute("USER_IN_SESSION", currentUser);
+            resp.sendRedirect("/employee");
+            return "";
+        }
+    }
 
+    /**
+     *用户注销
+     *用户注销功能的主要作用是保护用户的账户安全,在用户点击安全退出的时候,我们需要将本次会话相关的session信息删除
+     *删除的方式有下面两种:
+     *1.删除当前登录的用户信息
+     *存在问题:本次会话的其他信息还是保存在内存中,没有及时清理
+     */
+    @PostMapping("/logout")
+    public class LogoutServlet extends HttpServlet {
+        private IEmployeeService service = new EmployeeServiceImpl();
+        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            req.getSession().removeAttribute("USER_IN_SESSION");
+            resp.sendRedirect("/login.jsp");
+        }
+    }
 
+    /**
+     * 销毁整个session(推荐)
+     */
+
+    @WebServlet("/logout")
+    public class LogoutServlet extends HttpServlet {
+        private IEmployeeService service = new EmployeeServiceImpl();
+        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            req.getSession().invalidate();
+            resp.sendRedirect("/login.jsp");
+        }
     }
 }
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
-    private IEmployeeService service = new EmployeeServiceImpl();
 
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
-        String password = req.getParameter("password");
-        Employee currentUser = service.login(name, password);
-        if(currentUser==null){
-            //登录失败
-            req.setAttribute("errorMsg","亲,账户或者密码错误");
-            req.getRequestDispatcher("/login.jsp").forward(req,resp);
-            return;
-        }else{
-            //登录成功
-            req.getSession().setAttribute("USER_IN_SESSION",currentUser);
-            resp.sendRedirect("/employee");
-            return;
-        }
 
-        用户注销
-                用户注销功能的主要作用是保护用户的账户安全,在用户点击安全退出的时候,我们需要将本次会话相关的session信息删除
-        删除的方式有下面两种:
-        1.删除当前登录的用户信息
-        存在问题:本次会话的其他信息还是保存在内存中,没有及时清理
-        @WebServlet("/logout")
-        public class LogoutServlet extends HttpServlet {
-            private IEmployeeService service = new EmployeeServiceImpl();
-            protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                req.getSession().removeAttribute("USER_IN_SESSION");
-                resp.sendRedirect("/login.jsp");
-            }
-        }
 
-        2.销毁整个session(推荐)
-        @WebServlet("/logout")
-        public class LogoutServlet extends HttpServlet {
-            private IEmployeeService service = new EmployeeServiceImpl();
-            protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                req.getSession().invalidate();
-                resp.sendRedirect("/login.jsp");
-            }
-        }
+
+
